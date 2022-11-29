00f829773740faef7a8c4a5fa6cadd80feaacedd
Ryan <rmh.sturrock@gmail.com>
2022-11-29 16:15:51 -0600

    added change color button

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-alert('JS successfully linked!');
+
+let backgroundColor = document.querySelector('body');
+
+let changeColorButton = document.querySelector('button');
+
+
+changeColorButton.onclick = () => {
+
+    const colorArray = ['black','grey','red','yellow','green','purple','orange','violet','pink','teal','salmon'];
+
+    let randomIndex = Math.floor(Math.random() * colorArray.length);
+
+    backgroundColor.style.backgroundColor = colorArray[randomIndex];
+}
+
